5fe54ce6fabb8d1b7eb8b7bed2d68de67beee448
Subham mallick <chemsubham@gmail.com>
2025-01-18 12:50:56 +0530

    some fall back changes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import projectsRouter from "./routes/project.routes.js";
 
 const app = express();
 
-//Initialize dotenv configuration
-
+// Initialize dotenv configuration
 dotenv.config({
   path: "./.env",
 });
 
 // Initialize PORT
-
 const PORT = process.env.PORT || 8080;
 
-//CORS configuration
-
+// CORS configuration
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "*",
@@ -24,12 +22,18 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
-//Common Middleware
+
+// Common Middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-// routes
+// Routes
 app.use("/api/v1/projects", projectsRouter);
+
+// Fallback route for React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("client/build", "index.html"));
+});
 
 export { app, PORT };
